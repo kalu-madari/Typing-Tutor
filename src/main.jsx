d91621ab36a402f4241cf69ba6883a5f8519447e
kalu-madari <naveenjajranj@gmail.com>
2026-08-10 00:00:24 +0530

    fix: import light theme in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './css/styles.css'
-import './css/themes/midnight-indigo.css'
 import './css/themes/dark.css'
+import './css/themes/light.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
